b323ea53d9141ce7f331e55411c8c92814c14e6d
Gianfranco Malacalza <gianmlclz@gmail.com>
2025-02-02 19:12:37 -0300

    Creamos un footer

--- src/db/productos.js
+++ src/db/productos.js
@@ -3,63 +3,63 @@ const productos = [
     id: 1, 
     nombre: "Jamon De Cerdo", 
     foto: "imgs/jamon-de-cerdo.webp", 
-    descripcion: "Un jamón fresco y crudo de excelente calidad, color rosado brillante de grasa blanca y firme.", 
+    descripcion: "Un jamón de color rosado brillante de grasa blanca y firme.", 
     precio: "Precio por Kg: 3.931$"
     },
     { 
       id: 2, 
       nombre: "Paleta De Cerdo", 
       foto: "imgs/paleta-de-cerdo.webp", 
-      descripcion: "un corte selecto, suave y lleno de jugosidad. Perfecta para realzar el sabor de tus preparaciones más especiales.", 
+      descripcion: "un corte selecto, suave y lleno de jugosidad.", 
       precio: "Precio por Kg: 4.028$"
     },
     { 
       id: 3, 
       nombre: "Pecho Con Manta", 
       foto: "imgs/pecho-con-manta.webp", 
-      descripcion: "un corte tierno, jugoso y lleno de sabor, ideal para cocinar a fuego lento, asar o ahumar.", 
+      descripcion: "un corte tierno, ideal para cocinar a fuego lento, asar o ahumar.", 
       precio: "Precio por Kg: 4.565$"
     },
     { 
       id: 4, 
       nombre: "Carre Con Hueso", 
       foto: "imgs/carre-con-hueso.webp", 
-      descripcion: "un corte que proviene de la parte alta del lomo del cerdo. Incluye las costillas y una capa de grasa que aporta sabor y jugosidad", 
+      descripcion: "un corte jugoso y tierno con hueso expuesto, ideal para asados,", 
       precio: "Precio por Kg: 4.565$"
     },
     { 
       id: 5, 
       nombre: "Costeletas", 
       foto: "imgs/costeleta-De-Cerdo.webp", 
-      descripcion: "se distingue por su color rosado uniforme, grasa blanca brillante y textura suave, ideal para cocciones rápidas.", 
+      descripcion: "se distingue por su grasa brillante y suave sabor, ideal para cocciones rápidas.", 
       precio: "Precio por Kg: 5.098$"
     },
     {    
       id: 6, 
       nombre: "Bondiola", 
       foto: "imgs/bondiola-de-cerdo.webp", 
-      descripcion: "un corte jugoso y marmoleado, proveniente del cuello del cerdo. Se destaca por su textura tierna, sabor intenso y grasa equilibrada.", 
+      descripcion: "un corte jugoso y marmoleado, Se destaca por su sabor intenso y grasa equilibrada.", 
       precio: "Precio por Kg: 5.830$"
     },
     {    
       id: 7, 
       nombre: "Matambre", 
       foto: "imgs/matambre-de-cerdo.webp", 
-      descripcion: "un corte delgado y tierno, con grasa distribuida uniformemente. Se caracteriza por su color rosado y textura firme", 
+      descripcion: "un corte delgado y tierno, con grasa distribuida uniformemente.", 
       precio: "Precio por Kg: 8.197$"
     },
     {    
       id: 8, 
       nombre: "Panceta Piano Con Cuero", 
       foto: "imgs/panceta-piano.webp", 
-      descripcion: "un corte jugoso, con vetas marmoleadas que aportan sabor. Ideal para ahumar, asar o freír,", 
+      descripcion: "un corte jugoso, con vetas marmoleadas, Ideal para ahumar, asar o freír,", 
       precio: "Precio por Kg: 4.957$"
     },
     {    
       id: 9, 
       nombre: "Churrasco/Tapita", 
       foto: "imgs/churrasco-de-cerdo.webp", 
-      descripcion: "un corte magro. Ideal para parrilladas o cocciones rápidas, se destaca por su sabor delicado pero intenso.", 
+      descripcion: "un corte magro. Se destaca por su sabor delicado pero intenso.", 
       precio: "Precio por Kg: 4.841$"
     }, 
     {    
@@ -73,16 +73,16 @@ const productos = [
       id: 11, 
       nombre: "Tocino", 
       foto: "imgs/tocino.webp", 
-      descripcion: "un corte puro de grasa, con un sabor profundo. Su textura es firme pero se derrite fácilmente al cocinarlo, aportando un toque crujiente y sabroso a cualquier preparación.", 
+      descripcion: "un corte puro de grasa, Su textura es firme pero se derrite fácil al cocinarlo,", 
       precio: "Precio por Kg: 2.178$"
     },
     {    
       id: 12, 
       nombre: "Cuero De Lomo", 
       foto: "imgs/cuero-de-cerdo.webp", 
-      descripcion: " un corte limpio y bien procesado, con una textura firme y gruesa. Al cocinarlo, se vuelve crujiente por fuera y suave por dentro, ideal para preparar como chicharrón.", 
+      descripcion: " un corte limpio y bien procesado, ideal para preparar como chicharrón.", 
       precio: "Precio por Kg: 1.124$"
-    } 
+    },
     ]
 
 export default productos
